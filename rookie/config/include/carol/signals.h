b178e707f3495237b2ef5ffad24fe994449458c4
Markku Nissinen <markku.nissinen@ictnissinen.fi>
2020-04-26 06:06:32 -0400

    settings

--- rookie/config/include/carol/signals.h
+++ rookie/config/include/carol/signals.h
@@ -57,8 +57,32 @@ rookie_t;
 extern const rookie_t rookie;
 extern const iocDeviceHdr rookie_hdr;
 
+
+/* Array length defines. */
 #define ROOKIE_CONF_EXP_FRD_BUF_ARRAY_SZ 257
 #define ROOKIE_CONF_IMP_TOD_BUF_ARRAY_SZ 257
+
+/* Defines to check in code with #ifdef to know if signal is configured in JSON. */
+#define ROOKIE_EXP_LEFT
+#define ROOKIE_EXP_RIGHT
+#define ROOKIE_EXP_FORWARD
+#define ROOKIE_EXP_BACKWARD
+#define ROOKIE_IMP_LEFTTURN
+#define ROOKIE_IMP_RIGHTTURN
+#define ROOKIE_IMP_STRAIGHTFORWARD
+#define ROOKIE_IMP_FORWARDBACKWARD
+#define ROOKIE_CONF_EXP_TOD_STATE
+#define ROOKIE_CONF_EXP_TOD_TAIL
+#define ROOKIE_CONF_EXP_FRD_STATE
+#define ROOKIE_CONF_EXP_FRD_BUF
+#define ROOKIE_CONF_EXP_FRD_HEAD
+#define ROOKIE_CONF_IMP_TOD_CMD
+#define ROOKIE_CONF_IMP_TOD_SELECT
+#define ROOKIE_CONF_IMP_TOD_BUF
+#define ROOKIE_CONF_IMP_TOD_HEAD
+#define ROOKIE_CONF_IMP_FRD_CMD
+#define ROOKIE_CONF_IMP_FRD_SELECT
+#define ROOKIE_CONF_IMP_FRD_TAIL
 
 #ifndef IOBOARD_DEVICE_NAME
 #define IOBOARD_DEVICE_NAME "rookie"
